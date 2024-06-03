314b1b58b4a2625058d492b060fc1fb3033e7496
HEZRON ROTICH <hzron3@gmail.com>
2024-06-03 16:28:27 +0300

    Fix a bug

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ def main():
   print('Git is awesome!')
   print('Subscribe to Brief Channel!')
   print('Git is Engaging!')
+  print('Enjoy Awesome content!')
 
 if __name__ == '__main__':
   main()
